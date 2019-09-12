4e6f9dd5fa5c0792f2fc61797c96e6fd7ec7e518
bash4219 <kevin.bash18@gmail.com>
2019-09-12 07:57:59 -0500

    mawth

--- emojy.py
+++ emojy.py
@@ -5,6 +5,8 @@ scn = turtle.Screen()
 fas = turtle.Turtle()
 ies = turtle.Turtle()
 mawth = turtle.Turtle()
+ieeball = turtle.Turtle()
+
 
 def fase():
     fas.color("Yellow")
@@ -29,10 +31,25 @@ def iese():
     ies.begin_fill()
     ies.circle(20)
     ies.end_fill()
+    ies.goto(0, 0,)
+
+def mowth():
+    mawth.right(-90)
+    mawth.forward(90)
+    mawth.right(-90)
+    mawth.forward(90)
+    mawth.right(180)
+    mawth.forward(180)
+    mawth.forward(90)
+    for i in range(180):
+        mawth.right(1)
+        mawth.forward(1)
+
+
 
 
 fase()
 iese()
-
+mowth()
 
 turtle.exitonclick()
